ab8b107fb334913f200bc10db7fef1f12b2daa18
aarbid <aarbid@gmail.com>
2016-04-20 08:30:27 -0400

    Planes

--- Assets/Game.js
+++ Assets/Game.js
@@ -18,6 +18,9 @@ TrumpInvader.Game.prototype = {
     buildWorld: function() {
         this.add.image(0, 0, "EvilTrump");
         this.add.image(255, 975, "messagebox");
+        this.add.bitmapText(314, 996, "eightbitwonder", "In the year 3025, America")
+        this.add.bitmapText(314, 1036, "eightbitwonder", "has finally regained its former")
+        this.add.bitmapText(314, 1076, "eightbitwonder", "glory after years of cruel rule under supreme ruler Donald J. Trump.")
     },
     update: function() {},
 };
